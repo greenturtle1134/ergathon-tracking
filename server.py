e8557b17650ed8df1dbb042f8c7ecfcae1eede67
greenturtle1134 <daniel.y.fu@gmail.com>
2020-02-05 23:56:27 -0500

    First version of pi-to-server communication for testing

--- server.py
+++ server.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from flask import Flask, request
+from time import time
 app = Flask(__name__)
+
+distances = dict()
+goal = 0
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return str(distances)
+
+@app.route("/", methods=["PUT"])
+def update():
+    ergs = request.form['ergs']
+    data = request.form['data']
+    now = time()
+    for erg, val in zip(ergs, data):
+        if erg not in distances:
+            goal += val
+        distances[erg] = val, now
