a64e01608f703950e083f4fb17644af4a91f3388
greenturtle1134 <daniel.y.fu@gmail.com>
2020-02-06 14:04:40 -0500

    Updated recorder code to use JSON

--- erg_recorder.py
+++ erg_recorder.py
@@ -2,14 +2,8 @@ import requests
 
 SERVER = "https://ergathontracker.sites.tjhsst.edu/"
 
-def send_distances(data):
-    ergs = list()
-    distances = list()
-    for erg in data:
-        ergs.append(erg)
-        distances.append(data[erg])
-    return requests.put(SERVER, {"ergs":ergs, "data":distances})
-
-if __name__ == "__main__":
-    response = send_distances({"A":50, "B":100})
-    print(response)
+def send_distances(ergs):
+    data = list()
+    for erg in ergs:
+        data.append({"name":erg, "distance":ergs[erg]})
+    return requests.put(SERVER, json=data)
