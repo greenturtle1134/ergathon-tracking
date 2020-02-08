ba3957eec5a88d60454a6cd59b579a367159cd98
greenturtle1134 <daniel.y.fu@gmail.com>
2020-02-07 20:57:18 -0500

    First attempt at database use Also expanded recorder code

--- server.py
+++ server.py
@@ -1,18 +1,50 @@
-from flask import Flask, request
+from flask import Flask, request, current_app, g
+from flask.cli import with_appcontext
 from time import time
+import server_secret
+import psycopg2
+
 app = Flask(__name__)
+
+
+def get_db_cursor():
+    if 'db_conn' not in g:
+        g.db_conn = psycopg2.connect(user="site_ergathontracker", host="postgres1.csl.tjhsst.edu",
+                                     database="site_ergathontracker", password=server_secret.sql_password)
+    if 'db_cur' not in g:
+        g.db_cur = g.db_conn.cursor()
+    return g.db_cur
+
+
+@app.teardown_appcontext()
+def teardown_appcontext():
+    db_cur = g.pop("db_cur", None)
+    if db_cur is not None:
+        db_cur.close()
+    db_conn = g.pop("db_conn", None)
+    if db_conn is not None:
+        db_conn.commit()
+        db_conn.close()
 
 
 @app.route("/")
 def index():
     return "Hello World"
 
-@app.route("/", methods=["PUT"])
+
+@app.route("/ergs/", methods=["PUT"])
 def update():
+    cursor = get_db_cursor()
     count = 0
-    sum = 0
+    total = 0
     for erg in request.get_json():
         count += 1
-        sum += erg["distance"]
-        app.logger.info("Processed erg {} with distance {!s}".format(erg["name"], erg["distance"]))
-    return "{!s} ergs adding to {!s} meters".format(count, sum)
+        total += erg["distance"]
+        cursor.execute("INSERT INTO ergs (name, node, subnode, distance, last_update) "
+                       "VALUES (%s, %s, %s, %s, NOW()) "
+                       "ON CONFLICT ON CONSTRAINT unique_address "
+                       "DO UPDATE SET distance = EXCLUDED.distance, "
+                       "name = EXCLUDED.name, "
+                       "last_update = EXCLUDED.last_update",
+                       (erg["name"], erg["node"], erg["subnode"]. erg["distance"]))
+    return "{!s} ergs adding to {!s} meters".format(count, total)
